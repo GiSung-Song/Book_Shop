72981a231b5d329a044d0026abe433794843f334
GiSung-Song <wlswlek123@naver.com>
2021-12-15 14:07:06 +0900

    5장 완료

--- src/main/java/com/shop/shop/entity/Cart.java
+++ src/main/java/com/shop/shop/entity/Cart.java
@@ -11,14 +11,14 @@ import javax.persistence.*;
 @Getter
 @Setter
 @ToString
-public class Cart {
+public class Cart extends BaseEntity{
 
     @Id
     @Column(name = "cart_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @OneToOne //1:1매핑
+    @OneToOne(fetch = FetchType.LAZY) //1:1매핑, FetchType.Lazy 지연 로딩, 지연 로딩 설정 시 실제 엔티티 대신 HibernateProxy 객체를 넣어둠.
     @JoinColumn(name = "member_id") //외래키를 지정,
     private Member member;
 }
