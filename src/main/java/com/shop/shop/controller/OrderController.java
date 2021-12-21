1e1ee9c3b57261f7e6da9bdc4d0aced35c78d3cc
GiSung-Song <wlswlek123@naver.com>
2021-12-21 17:17:56 +0900

    7장 finish
    7장 상품 주문

---
+++ src/main/java/com/shop/shop/controller/OrderController.java
@@ -0,0 +1,85 @@
+package com.shop.shop.controller;
+
+import com.shop.shop.dto.OrderDto;
+import com.shop.shop.dto.OrderHistDto;
+import com.shop.shop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    /**
+     * 비동기 방식(새로 고침 없이 서버에 요청하기 위해)
+     *
+     * @ReqeustBody : HTTP 요청의 body에 남긴 내용을 자바 객체로 전달
+     * @ResponseBody : 자바 객체를 HTTP 요청의 body로 전달
+     */
+
+    @PostMapping("/order")
+    public @ResponseBody
+    ResponseEntity order(@RequestBody @Valid OrderDto orderDto,
+                         BindingResult bindingResult, Principal principal) {
+
+        if (bindingResult.hasErrors()) {
+            StringBuilder sb = new StringBuilder();
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            for (FieldError fieldError : fieldErrors) {
+                sb.append(fieldError.getDefaultMessage());
+            }
+            return new ResponseEntity<String>(sb.toString(), HttpStatus.BAD_REQUEST);
+        }
+
+        String email = principal.getName(); //현재 로그인한 회원의 이메일 정보를 조회(spring security)
+        Long orderId;
+
+        try {
+            orderId = orderService.order(orderDto, email);
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<Long>(orderId, HttpStatus.OK);
+
+    }
+
+    @GetMapping(value = {"/orders", "/orders/{page}"})
+    public String orderHist(@PathVariable("page") Optional<Integer> page, Principal principal, Model model) {
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 4);
+
+        Page<OrderHistDto> orderHistDtoList = orderService.getOrderList(principal.getName(), pageable);
+
+        model.addAttribute("orders", orderHistDtoList);
+        model.addAttribute("page", pageable.getPageNumber());
+        model.addAttribute("maxPage", 5);
+
+        return "order/orderHist";
+    }
+
+    @PostMapping("/order/{orderId}/cancel")
+    public @ResponseBody ResponseEntity cancelOrder(@PathVariable("orderId") Long orderId, Principal principal) {
+        if(!orderService.validateOrder(orderId, principal.getName())) {
+            return new ResponseEntity<String>("주문 취소 권한이 없습니다.", HttpStatus.FORBIDDEN);
+        }
+
+        orderService.cancelOrder(orderId);
+        return new ResponseEntity<Long>(orderId, HttpStatus.OK);
+    }
+}
--- src/main/java/com/shop/shop/entity/Item.java
+++ src/main/java/com/shop/shop/entity/Item.java
@@ -2,6 +2,8 @@ package com.shop.shop.entity;
 
 import com.shop.shop.constant.ItemSellStatus;
 import com.shop.shop.dto.ItemFormDto;
+import com.shop.shop.exception.OutOfStockException;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -43,5 +45,17 @@ public class Item extends BaseEntity{
         this.stockNumber = itemFormDto.getStockNumber();
         this.itemDetail = itemFormDto.getItemDetail();
         this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
+
+    public void removeStock(int stockNumber) {
+        int restStock = this.stockNumber - stockNumber;
+        if(restStock < 0) {
+            throw new OutOfStockException("상품의 재고가 부족합니다. (현재 재고 수량 : " + this.stockNumber + ")");
+        }
+        this.stockNumber = restStock;
+    }
+
+    public void addStock(int stockNumber) {
+        this.stockNumber += stockNumber;
     }
 }
